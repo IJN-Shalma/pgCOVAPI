22fe838121395f6ee5c55e0ae7ed182a3ac78cec
Silvio Caprara <53000709+IJN-Shalma@users.noreply.github.com>
2021-04-02 15:10:41 +0200

    Update regione.js

--- routes/regione.js
+++ routes/regione.js
@@ -9,10 +9,18 @@ router.route('/').get((req, res) => {
 
 
 router.route('/:regione').get((req, res) => {
-    Regione.find({'denominazione_regione': req.params.regione})
+
+    const periodo = (req.query.mese).split('-') || null;
+    //periodo[0] = mese
+    //periodo[1] = anno
+
+    Regione.find(
+        { 'denominazione_regione': req.params.regione },
+        { 'data': new Date(periodo[1] + "-" + periodo[0]) })
         .then(regione => res.json(regione))
         .catch(err => res.status(400).json('Error: ') + err);
 });
+
 
 
 module.exports = router;
