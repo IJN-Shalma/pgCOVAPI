f0a877da8bd47bce4dd6439ddc0c7a2865dd9ab3
Silvio Caprara <53000709+IJN-Shalma@users.noreply.github.com>
2021-04-02 15:43:39 +0200

    fix route problems

--- routes/api/regione.js
+++ routes/api/regione.js
@@ -1,19 +1,21 @@
 const router = require('express').Router();
-let Regione = require("../../models/regione.model");
+let Regione = require("../../models/regione.model")
 
 router.route('/').get((req, res) => {
     Regione.find()
-        .sort({data: 1, denominazione_regione: 1})
         .then(regione => res.json(regione))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
 
 router.route('/:regione').get((req, res) => {
-    Regione.find({'denominazione_regione': req.params.regione})
+
+    Regione.find(
+        { 'denominazione_regione': req.params.regione })
         .then(regione => res.json(regione))
         .catch(err => res.status(400).json('Error: ') + err);
 });
+
 
 
 module.exports = router;
