5c3655f2beac5ee92974e9c0b820e63c0e154c19
pabalaba <chiartano.pietro@gmail.com>
2023-10-13 21:12:02 +0200

    Feat. added pipeline and dockerfiles

--- routes/api/root.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-router.route('/').get((req, res) => {
-
-    res.status(200).json('{documentation_link: "https://ijn-shalma.github.io/pgCOVAPI/"}');
-});
-
-module.exports = router;
